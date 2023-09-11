e04e21ce78d7211757ed51ca666d9108d3ec837b
vishnu-nz <vishnuaddagada.nz@gmail.com>
2023-09-12 03:59:21 +0530

    AssignmentFiles

--- Plus500_CaptureStockValues_Assignment2/.local/install/CodedWorkflow.cs
+++ Plus500_CaptureStockValues_Assignment2/.local/install/CodedWorkflow.cs
@@ -19,7 +19,7 @@ namespace Plus500_CaptureStockValues_Assignment2
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
